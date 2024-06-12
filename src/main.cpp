4873c7e2a0e1978325485b64d326ed2e6661962f
lailokkei <lailokkay@gmail.com>
2024-06-12 14:57:49 +0100

    add editor

--- src/main.cpp
+++ src/main.cpp
@@ -63,6 +63,7 @@ public:
     Vec2 m_Bounds;
     
     Vec2 WorldToScreen(Vec2 pos) const;
+    float world_to_screen(float length) const;
     Vec2 screen_to_world(Vec2 pos) const;
 };
 
@@ -71,9 +72,14 @@ Vec2 Cam::WorldToScreen(Vec2 pos) const {
     Vec2 normalized = temp / m_Bounds + 0.5;
         
     return Vec2 {
-        normalized.x * GetScreenWidth(),
-        normalized.y * GetScreenHeight()
+        normalized.x * (float)GetScreenWidth(),
+        normalized.y * (float)GetScreenHeight()
     };
+}
+
+float Cam::world_to_screen(float length) const {
+    return length / m_Bounds.y * GetScreenHeight();
+
 }
 
 Vec2 Cam::screen_to_world(Vec2 pos) const {
@@ -81,6 +87,7 @@ Vec2 Cam::screen_to_world(Vec2 pos) const {
     Vec2 offset = (normalized - Vec2{0.5, 0.5}) * m_Bounds;
     return {offset.x + m_Position.x, m_Position.y - offset.y};
 }
+
 
 class GameState {
 public:
@@ -132,8 +139,6 @@ void GameState::Update(std::chrono::duration<double> delta_time) {
 
     std::vector<NoteType> pressed;
 
-
-
     if (IsKeyPressed(KEY_X)) {
         PlaySound(don_sound);
 
@@ -151,8 +156,6 @@ void GameState::Update(std::chrono::duration<double> delta_time) {
     }
 
     for (auto& note : pressed) {
-        std::cout << (elapsed - map[current_note].time).count() << "\n";
-
         if ((elapsed - map[current_note].time) / hit_range + 0.5 < 1) {
             if (map[current_note].type == note) {
                 current_note++;
@@ -182,35 +185,77 @@ void GameState::Update(std::chrono::duration<double> delta_time) {
 
 }
 
-void render_map(const std::vector<Note>& map, const Cam& cam) {
-    for(int i = 0; i < map.size(); i++) {
+void render_map(const std::vector<Note>& map, const Cam& cam, int current_note) {
+    if (map.size() == 0) {
+        return;
+    }
+
+    int right = map.size() - 1;
+    int left = 0;
+    constexpr float circle_padding = 0.2f;
+    float right_bound = cam.m_Position.x + cam.m_Bounds.x / 2 + circle_padding;
+    float left_bound = cam.m_Position.x - (cam.m_Bounds.x / 2 + circle_padding);
+
+    for (int i = current_note; i < map.size(); i++) {
+        if (map[i].time.count() >= right_bound) {
+            right = i - 1;
+            break;
+        }
+    }
+    for (int i = current_note; i >= 0; i--) {
+        if (i >= map.size()) {
+            continue;
+        }
+        if (map[i].time.count() <= left_bound) {
+            left = i + 1;
+            break;
+        }
+    }
+
+    for(int i = right; i >= left; i--) {
         const Note& note = map[i];
         Vec2 circle_pos = cam.WorldToScreen({(float)note.time.count(), 0});
         Color color = (note.type == NoteType::don) ? RED : BLUE;
 
-        DrawCircle(circle_pos.x, circle_pos.y, 20, color);
+        DrawCircle(circle_pos.x, circle_pos.y, cam.world_to_screen(0.11), WHITE);
+        DrawCircle(circle_pos.x, circle_pos.y, cam.world_to_screen(0.1), color);
     }
 }
 
-class Editor {
+enum class EditorMode {
+    select,
+    insert,
+};
+
+class App {
 public:
     void Update(std::chrono::duration<double> delta_time);
     void Init();
 private:
     Cam cam = {{0,0}, {2,3}};
 
+    EditorMode mode;
     std::vector<int> selected;
+    NoteType note_type;
 
     std::vector<Note> map;
     std::chrono::duration<double> offset;
     float bpm = 253;
 
+    double quarter_interval = 60 / bpm / 4;
+    double collision_range = quarter_interval / 2;
+
     bool paused = true;
 
+    int current_note = -1;
+
     Music music;
+
+    Sound don_sound = LoadSound("don.wav");
+    Sound kat_sound = LoadSound("kat.wav");
 };
 
-void Editor::Init() {
+void App::Init() {
     music = LoadMusicStream("kinoko.mp3");
     SetMusicVolume(music, 0.2f);
 
@@ -220,29 +265,56 @@ void Editor::Init() {
     PauseMusicStream(music);
 
     SeekMusicStream(music, 0.0f);
-
-    map.push_back({std::chrono::duration<double>(1), NoteType::don});
-    selected.push_back(0);
 }
 
-void Editor::Update(std::chrono::duration<double> delta_time) {
+void add_note(std::vector<Note>& map, Note note) {
+    auto cmp = [](Note l, Note r) { return l.time < r.time; };
+    map.insert(std::upper_bound(map.begin(), map.end(), note, cmp), note);
+}
+
+void App::Update(std::chrono::duration<double> delta_time) {
     UpdateMusicStream(music);
     BeginDrawing();
     
     ClearBackground(BLACK);
 
+    float elapsed = GetMusicTimePlayed(music);
+
     if (IsMouseButtonPressed(0)) {
         Vec2 cursor_pos = cam.screen_to_world({(float)GetMouseX(), (float)GetMouseY()});
-        std::cout << std::format("{} {}\n", cursor_pos.x, cursor_pos.y);
 
-        map.push_back({
-            std::chrono::duration<double>(cursor_pos.x),
-            NoteType::don
-        });
+        int i = std::round((cursor_pos.x - offset.count()) / quarter_interval);
+        auto time = std::chrono::duration<double>(offset.count() + i * quarter_interval);
+
+        bool collision = false;
+        double diff;
+        for (int i = 0; i < map.size(); i++) {
+            diff = std::abs((map[i].time - time).count());
+            if (diff < collision_range) {
+                collision = true;
+                break;
+            }
+        }
+
+        if (!collision) {
+            add_note(map, Note{time, note_type});
+
+            if (time.count() < elapsed) {
+                current_note++;
+            }
+        }
+    }
+
+    if (IsKeyPressed(KEY_R)) {
+        if (note_type == NoteType::don) {
+            note_type = NoteType::kat;
+        } else {
+            note_type = NoteType::don;
+        }
     }
 
     for (int i = 0; i < 500; i++) {
-        float x = offset.count() + i * 60 / bpm / 4;
+        float x = offset.count() + i * quarter_interval;
 
         float height;
         Color color;
@@ -265,9 +337,13 @@ void Editor::Update(std::chrono::duration<double> delta_time) {
 
     DrawLine(p1.x, p1.y, p2.x, p2.y, YELLOW);
 
+
     if (IsKeyPressed(KEY_SPACE)) {
         if (paused) {
             ResumeMusicStream(music);
+            auto cmp = [](float current_time, Note& note) { return current_time < note.time.count(); };
+            current_note = std::upper_bound(map.begin(), map.end(), elapsed, cmp) - map.begin();
+            std::cout << current_note << '\n';
         } else {
             PauseMusicStream(music);
         }
@@ -275,28 +351,45 @@ void Editor::Update(std::chrono::duration<double> delta_time) {
         paused = !paused;
     }
 
+    if (!paused && current_note < map.size() && elapsed >= map[current_note].time.count()) {
+        if (map[current_note].type == NoteType::don) {
+            PlaySound(don_sound);
+        } else {
+            PlaySound(kat_sound);
+        }
+
+        current_note++;
+    }
+
     if (IsKeyPressed(KEY_A) && GetMusicTimePlayed(music) != 0.0f) {
         SeekMusicStream(music, 1.0f);
         std::cout << GetMusicTimePlayed(music) << '\n';
     }
 
-    float played = GetMusicTimePlayed(music);
-    cam.m_Position.x = played;
+    cam.m_Position.x = elapsed;
 
     float wheel = GetMouseWheelMove();
     if (wheel != 0) {
-        float seek = std::clamp(played - wheel * 0.4f, 0.0f, GetMusicTimeLength(music));
-        // std::cout << GetMusicTimeLength(music) << '\n';
+        float seek = std::clamp(elapsed - wheel * 0.4f, 0.0f, GetMusicTimeLength(music));
         SeekMusicStream(music, seek);
-        std::cout << seek << '\t' << GetMusicTimePlayed(music) << '\n';
+        elapsed = GetMusicTimePlayed(music);
+
+        // if (map.size() > 0 && elapsed < map[current_note].time.count()) {
+        //     for (int i = current_note - 1; i >= 0; i--) {
+        //         if (elapsed > map[i].time.count()) {
+        //             current_note = i + 1;
+        //             break;
+        //         }
+        //     }
+        // } else {
+
+        // }
+        // auto cmp = [](float current_time, Note& note) { return current_time < note.time.count(); };
+        // current_note = std::upper_bound(map.begin(), map.end(), elapsed, cmp) - map.begin();
+        std::cout << current_note << '\n';
     }
 
-    for (int& i : selected) {
-        Vec2 circle_pos = cam.WorldToScreen({(float)map[i].time.count(), 0});
-        DrawCircle(circle_pos.x, circle_pos.y, 21, WHITE);
-    }
-
-    render_map(map, cam);
+    render_map(map, cam, current_note);
 
 
     DrawText(std::to_string(cam.m_Position.x).data(), 0, 0, 24, WHITE);
@@ -304,48 +397,36 @@ void Editor::Update(std::chrono::duration<double> delta_time) {
     EndDrawing();
 }
 
-
-int main() {
-    InitWindow(800, 600, "taiko");
+void run() {
+    InitWindow(1280, 960, "taiko");
     
     auto last_frame = std::chrono::high_resolution_clock::now();
     
     InitAudioDevice();
     
-    
-    // for (const auto& entry : std::filesystem::directory_iterator(".")) {
-    //     auto file_name = entry.path().filename();
-    //     std::cout << file_name << "\n";
-    // }
-    
-    // Music music = LoadMusicStream("mesmerizer.mp3");
-    // PlayMusicStream(music);
-
-
-
-
     float pos = 400;
-
-
-
     auto start = std::chrono::high_resolution_clock::now();
 
     GameState game_state;
 
-    Editor editor;
+    App app;
 
-    editor.Init();
+    app.Init();
 
     using namespace std::chrono;
     
     while (!WindowShouldClose()) {
         auto now = high_resolution_clock::now();
         duration<double> delta_time = now - last_frame;
-        editor.Update(delta_time);
+        app.Update(delta_time);
 
 
         last_frame = now;
     }
     
     CloseWindow();
+}
+
+int main() {
+    run();
 }
