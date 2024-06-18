194eb7f7e773a0cec97b7ebef05a4a5346f746cf
lailokkei <lailokkay@gmail.com>
2024-06-18 17:01:15 +0100

    ui sliders

--- src/ui.cpp
+++ src/ui.cpp
@@ -2,10 +2,16 @@
 #include "raylib.h"
 #include <iostream>
 
-void UI::begin_group() {
-    groups.push_back(Group{});
+void UI::begin_group(Style style) {
+    groups.push_back(Group{ {},  style });
     in_group = true;
 }
+
+//Element UI::get_element(ElementHandle handle) {
+//    switch (handle.type) {
+//        case 
+//    }
+//}
 
 void UI::end_group() {
     in_group = false;
@@ -13,26 +19,67 @@ void UI::end_group() {
     Group& group = groups[groups.size() - 1];
 
 	float total_width = 50;
-	for (auto& i: group.children) {
-		total_width += rects[i].scale.x;
+	for (auto& e: group.children) {
+        switch (e.type) {
+        case ElementType::slider:
+            total_width += sliders[e.index].scale.x;
+            break;
+        case ElementType::button:
+            total_width += buttons[e.index].scale.x;
+            break;
+        case ElementType::rect:
+            total_width += rects[e.index].scale.x;
+            break;
+
+        }
 	}
+
     float total_height = font_size;
 
 	Vec2 start = {((float)GetScreenWidth() - total_width) * group.style.anchor.x, ((float)GetScreenHeight() - total_height) * group.style.anchor.y};
 
-	for (auto& i: group.children) {
-		Rect& rect = rects[i];
-		rect.position = {start.x, start.y};
+	for (auto& e: group.children) {
+        switch (e.type) {
+        case ElementType::slider:
+        {
+            Slider& slider = sliders[e.index];
+			slider.position = {start.x, start.y};
 
-        start.x += rect.scale.x + 50;
+			const Vec2& p1 = slider.position;
+			Vec2 p2 = slider.position + slider.scale;
 
-		const Vec2& p1 = rect.position;
-		const Vec2& p2 = rect.position + rect.scale;
-
-        if (rect.callback.has_value()) {
 			if (mouse_zero && mouse_pos.x > p1.x && mouse_pos.y > p1.y && mouse_pos.x < p2.x && mouse_pos.y < p2.y) {
-				rect.callback.value()();
+                slider.callback((mouse_pos.x - start.x) / slider.scale.x);
 			}
+
+			start.x += slider.scale.x + 50;
+
+            break;
+        }
+        case ElementType::button:
+        {
+            Button& button = buttons[e.index];
+			button.position = {start.x, start.y};
+
+			const Vec2& p1 = button.position;
+			Vec2 p2 = button.position + button.scale;
+
+			if (mouse_zero && mouse_pos.x > p1.x && mouse_pos.y > p1.y && mouse_pos.x < p2.x && mouse_pos.y < p2.y) {
+                button.callback();
+			}
+
+			start.x += button.scale.x + 50;
+
+            break;
+        }
+        case ElementType::rect:
+        {
+			Rect& rect = rects[e.index];
+			rect.position = {start.x, start.y};
+			start.x += rect.scale.x + 50;
+
+            break;
+        }
         }
 	}
 }
@@ -43,32 +90,43 @@ void UI::input() {
 }
 
 void UI::rect(const char* text) {
-    rects.push_back(Rect{ Vec2{0, 0}, Vec2{(float)MeasureText(text, font_size), (float)font_size}, text, {}});
+    rects.push_back(Rect{ Vec2{0, 0}, Vec2{(float)MeasureText(text, font_size), (float)font_size}, text});
 
     if (in_group) {
-        groups[groups.size() - 1].children.push_back(rects.size() - 1);
+        groups[groups.size() - 1].children.push_back(ElementHandle{ ElementType::rect, rects.size() - 1 });
+    }
+}
+
+
+void UI::slider(float fraction, std::function<void(float)> on_input) {
+    sliders.push_back({ {}, {500, 36}, fraction, on_input });
+
+    if (in_group) {
+        groups[groups.size() - 1].children.push_back({ ElementType::slider, sliders.size() - 1 });
     }
 }
 
 void UI::button(const char* text, std::function<void()> on_click) {
-    float height = 0;
-    if (rects.size() > 0) {
-        height = rects[rects.size() - 1].position.y + 220;
-    }
-
-    rects.push_back(Rect{Vec2{0, height}, Vec2{(float)MeasureText(text, font_size), (float)font_size}, text, on_click});
+    buttons.push_back( {Vec2{0, 0}, Vec2{(float)MeasureText(text, font_size), (float)font_size}, text, on_click});
 
     if (in_group) {
-        groups[groups.size() - 1].children.push_back(rects.size() - 1);
+        groups[groups.size() - 1].children.push_back({ ElementType::button, buttons.size() - 1 });
     }
 }
 
 void UI::draw() {
-    for (auto& r : rects) {
-        DrawRectangle(r.position.x, r.position.y, r.scale.x, r.scale.y, GRAY);
-        DrawText(r.text, r.position.x, r.position.y, font_size, WHITE);
+    for (auto& e : rects) {
+        DrawRectangle(e.position.x, e.position.y, e.scale.x, e.scale.y, GRAY);
+        DrawText(e.text, e.position.x, e.position.y, font_size, WHITE);
+    }
+
+    for (auto& e : sliders) {
+        DrawRectangle(e.position.x, e.position.y, e.scale.x, e.scale.y, GRAY);
+        float x = e.position.x + e.scale.x * e.fraction;
+        DrawLine(x, e.position.y, x, e.position.y + e.scale.y, YELLOW);
     }
 
     rects.clear();
+    sliders.clear();
     groups.clear();
 }
--- src/ui.h
+++ src/ui.h
@@ -1,9 +1,14 @@
-#include <vector>
+﻿#include <vector>
 #include <functional>
 #include <optional>
 #include "vec.h"
 
 #pragma once
+
+
+class Element {
+    virtual Vec2 position() = 0;
+};
 
 struct Style {
     Vec2 anchor;
@@ -13,28 +18,56 @@ struct Rect {
     Vec2 position;
     Vec2 scale;
     const char* text;
-    std::optional<std::function<void()>> callback;
+};
+
+struct Button {
+    Vec2 position;
+    Vec2 scale;
+    const char* text;
+    std::function<void()> callback;
+};
+
+struct Slider {
+    Vec2 position;
+    Vec2 scale;
+    float fraction;
+    std::function<void(float)> callback;
+};
+
+enum class ElementType {
+    button,
+    slider,
+    rect,
+};
+
+struct ElementHandle {
+    ElementType type;
+    size_t index;
 };
 
 struct Group {
-    std::vector<int> children;
-    Style style = { Vec2{1.0f,1.0f} };
+    std::vector<ElementHandle> children;
+    Style style =  {};
 };
+
 
 class UI {
 public:
     void button(const char* text, std::function<void()> on_click);
+    void slider(float fraction, std::function<void(float)> on_input);
     void rect(const char* text);
 
     void input();
     void draw();
 
-    void begin_group();
+    void begin_group(Style style);
     void end_group();
 
 private:
-	const int font_size = 36;
+    const int font_size = 36;
+    std::vector<Button> buttons;
     std::vector<Rect> rects;
+    std::vector<Slider> sliders;
     std::vector<Group> groups;
 
     bool mouse_zero;
