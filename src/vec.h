e36e55f59ceaac40b965aafbad56acc4ca21768b
lailokkei <lailokkay@gmail.com>
2024-06-18 17:01:02 +0100

    vector +=

--- src/vec.h
+++ src/vec.h
@@ -11,6 +11,12 @@ struct Vec2 {
     Vec2 operator+(const float& other) {
         return Vec2{x + other, y + other};
     }
+
+    Vec2& operator+=(const Vec2& rhs) {
+        this->x += rhs.x;
+        this->y += rhs.y;
+        return *this;
+    }
     
     Vec2 operator-(const Vec2& other) {
         return Vec2{x - other.x, y - other.y};
