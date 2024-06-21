52c362d58fd1f0eef782302d65ebdf5611f338cd
lailokkei <lailokkay@gmail.com>
2024-06-21 19:17:19 +0100

    fix vec overloads

--- src/vec.h
+++ src/vec.h
@@ -1,40 +1,40 @@
 #pragma once
 
 struct Vec2 {
-    float x;
-    float y;
+	float x;
+	float y;
 
-    Vec2 operator+(const Vec2& other) {
-        return Vec2{x + other.x, y + other.y};
-    }
+	static Vec2 one() {
+		return { 1, 1 };
+	}
 
-    Vec2 operator+(const float& other) {
-        return Vec2{x + other, y + other};
-    }
-
-    Vec2& operator+=(const Vec2& rhs) {
-        this->x += rhs.x;
-        this->y += rhs.y;
-        return *this;
-    }
-    
-    Vec2 operator-(const Vec2& other) {
-        return Vec2{x - other.x, y - other.y};
-    }
-
-    Vec2 operator*(const float& other) {
-        return Vec2{x * other, y * other};
-    }
-
-    Vec2 operator/(const float& other) {
-        return Vec2{x / other, y / other};
-    }
-
-    Vec2 operator/(const Vec2& other) {
-        return Vec2{x / other.x, y / other.y};
-    }
-
-    Vec2 operator*(const Vec2& other) {
-        return Vec2{x * other.x, y * other.y};
-    }
+	Vec2& operator+=(const Vec2& rhs) {
+		x += rhs.x;
+		y += rhs.y;
+		return *this;
+	}
 };
+
+inline Vec2 operator+(const Vec2& lhs, const Vec2& rhs) {
+	return Vec2{ lhs.x + rhs.x, lhs.y + rhs.y };
+}
+
+inline Vec2 operator-(const Vec2& lhs, const Vec2& rhs) {
+	return Vec2{ lhs.x - rhs.x, lhs.y - rhs.y };
+}
+
+inline Vec2 operator*(const Vec2& lhs, const float& rhs) {
+	return Vec2{ lhs.x * rhs, lhs.y * rhs };
+}
+
+inline Vec2 operator/(const Vec2& lhs, const float& rhs) {
+	return Vec2{ lhs.x / rhs, lhs.y / rhs };
+}
+
+inline Vec2 operator*(const Vec2& lhs, const Vec2& rhs) {
+	return Vec2{ lhs.x * rhs.x, lhs.y * rhs.y };
+}
+
+inline Vec2 operator/(const Vec2& lhs, const Vec2& rhs) {
+	return Vec2{ lhs.x / rhs.x, lhs.y / rhs.y };
+}
