cb19f11b7973f0ab2648ad9a0c89080828b75b3a
lailokkei <lailokkay@gmail.com>
2024-06-21 19:17:38 +0100

    editor selection

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ const int window_width = 1280;
 const int window_height = 960;
 
 const std::chrono::duration<double> hit_range = 100ms;
-const std::chrono::duration<double> perfecct_range = 30ms;
+const std::chrono::duration<double> perfect_range = 30ms;
 
 const float circle_radius = 0.1f;
 const float circle_outer_radius = 0.11f;
@@ -27,6 +27,8 @@ struct Textures {
 	Texture2D circle_overlay;
 	Texture2D big_circle;
 	Texture2D big_circle_overlay;
+
+    Texture2D select_circle;
 
 	Texture2D inner_drum;
 	Texture2D outer_drum;
@@ -57,30 +59,34 @@ public:
     Vec2 position;
     Vec2 bounds;
     
-    Vec2 world_to_screen(Vec2 pos) const;
-    float world_to_screen_scale(float length) const;
-    Vec2 screen_to_world(Vec2 pos) const;
+    Vec2 world_to_screen(const Vec2& point) const;
+    Vec2 screen_to_world(const Vec2& point) const;
+    Vec2 world_to_screen_scale(const Vec2& scale) const;
+    float world_to_screen_scale(const float& length) const;
 };
 
-Vec2 Cam::world_to_screen(Vec2 pos) const {
-    Vec2 temp = {pos.x - position.x, position.y - pos.y};
-    Vec2 normalized = temp / bounds + 0.5;
+Vec2 Cam::world_to_screen(const Vec2& point) const {
+    Vec2 relative_pos = {point.x - position.x, position.y - point.y};
+    Vec2 normalized = relative_pos / bounds + Vec2::one() * 0.5f; // 0 to 1
         
-    return Vec2 {
+    return Vec2 {	
         normalized.x * (float)GetScreenWidth(),
         normalized.y * (float)GetScreenHeight()
     };
 }
+Vec2 Cam::world_to_screen_scale(const Vec2& scale) const {
+    return { world_to_screen_scale(scale.x), world_to_screen_scale(scale.y) };
+}
 
-float Cam::world_to_screen_scale(float length) const {
+float Cam::world_to_screen_scale(const float& length) const {
     return length / bounds.y * GetScreenHeight();
 
 }
 
-Vec2 Cam::screen_to_world(Vec2 pos) const {
-    Vec2 normalized = {pos.x / GetScreenWidth(), pos.y / GetScreenHeight()};
-    Vec2 offset = (normalized - Vec2{0.5, 0.5}) * bounds;
-    return {offset.x + position.x, position.y - offset.y};
+Vec2 Cam::screen_to_world(const Vec2& point) const {
+    Vec2 normalized = { point.x / GetScreenWidth(), point.y / GetScreenHeight() };
+    Vec2 relative_pos = (normalized - Vec2::one() * 0.5f) * bounds;
+    return { relative_pos.x + position.x, position.y - relative_pos.y };
 }
 
 void draw_map(const std::vector<Note>& map, const Cam& cam, int current_note) {
@@ -159,12 +165,14 @@ public:
     
     std::vector<float> times;
     std::vector<NoteFlags> flags_vec;
+    std::vector<bool> selected;
 };
 
 void Map::insert_note(float time, NoteFlags flags) {
     int i = std::upper_bound(times.begin(), times.end(), time) - times.begin();
     times.insert(times.begin() + i, time);
     flags_vec.insert(flags_vec.begin() + i, flags);
+    selected.insert(selected.begin() + i, false);
 }
 
 
@@ -339,6 +347,72 @@ void Game::update(std::chrono::duration<double> delta_time) {
 
 }
 
+const Vec2 note_hitbox = { 0.5, 0.5 };
+
+std::vector<int> note_box_intersection(const Map& map, Vec2 start_pos, Vec2 end_pos) {
+	if (end_pos.x < start_pos.x) {
+		float temp = start_pos.x;
+		start_pos.x = end_pos.x;
+		end_pos.x = temp;
+	}
+	
+    if (end_pos.y > start_pos.y) {
+        float temp = start_pos.y;
+        start_pos.y = end_pos.y;
+        end_pos.y = temp;
+    }
+
+    std::vector<int> hits;
+
+    for (int i = 0; i < map.times.size(); i++) {
+        const float& x = map.times[i];
+        if (x > start_pos.x && x < end_pos.x && start_pos.y > 0 && end_pos.y < 0) {
+            hits.push_back(i);
+        }
+    }
+ 
+    return hits;
+}
+
+std::optional<int> note_point_intersection(const Map& map, const Vec2& point, const int& current_note) {
+    Vec2 half_bounds = note_hitbox / 2;
+    std::vector<int> hits;
+    for (int i = 0; i < map.times.size(); i++) {
+        Vec2 center = { map.times[i], 0 };
+        Vec2 top_left = { center.x - half_bounds.x, center.y + half_bounds.y };
+        Vec2 bottom_right = { center.x + half_bounds.x, center.y - half_bounds.y };
+
+        if (point.x > top_left.x && point.y < top_left.y && point.x < bottom_right.x && point.y > bottom_right.y) {
+            hits.push_back(i);
+        }
+
+    }
+
+    if (hits.size() == 0) {
+        return {};
+    }
+
+    int min = hits[0];
+
+    for (int i = 1; i < hits.size(); i++) {
+        if (std::abs(map.times[hits[i]] - point.x) < std::abs(map.times[min] - point.x)) {
+            min = hits[i];
+        }
+    }
+
+    return min;
+}
+
+void draw_wire_box(const Vec2& pos, const Vec2& scale, const Color& color) {
+    Vec2 p2 = pos + Vec2{ 1, 0 } * scale;
+    Vec2 p3 = pos + scale;
+    Vec2 p4 = pos + Vec2{ 0, 1 } * scale;
+    DrawLine(pos.x, pos.y, p2.x, p2.y, color);
+    DrawLine(p2.x, p2.y, p3.x, p3.y, color);
+    DrawLine(p3.x, p3.y, p4.x, p4.y, color);
+    DrawLine(p4.x, p4.y, pos.x, pos.y, color);
+}
+
 void draw_map_editor(const Map& map, const Cam& cam, int current_note) {
     if (map.times.size() == 0) {
         return;
@@ -367,16 +441,25 @@ void draw_map_editor(const Map& map, const Cam& cam, int current_note) {
     }
 
     for(int i = right; i >= left; i--) {
-        Vec2 circle_pos = cam.world_to_screen({map.times[i], 0});
+        Vec2 center_pos = cam.world_to_screen({map.times[i], 0});
         float scale = (map.flags_vec[i] & NoteFlagBits::normal_or_big) ? 0.9f : 1.4f;
+        Vec2 circle_pos = center_pos;
         circle_pos.x -= textures.circle.width / 2 * scale;
         circle_pos.y -= textures.circle.height / 2 * scale;
         Color color = (map.flags_vec[i] & NoteFlagBits::don_or_kat) ? RED : BLUE;
 
+        Vec2 select_pos = { center_pos.x - textures.select_circle.width / 2, center_pos.y - textures.select_circle.height / 2};
+
         DrawTextureEx(textures.circle, { circle_pos.x, circle_pos.y }, 0, scale, color);
         DrawTextureEx(textures.circle_overlay, { circle_pos.x, circle_pos.y }, 0, scale, WHITE);
-        //DrawCircle(circle_pos.x, circle_pos.y, cam.world_to_screen_scale(circle_outer_radius), WHITE);
-        //DrawCircle(circle_pos.x, circle_pos.y, cam.world_to_screen_scale(circle_radius), color);
+
+        Vec2 hitbox_bounds = cam.world_to_screen_scale(note_hitbox);
+        Vec2 hitbox_pos = center_pos - (hitbox_bounds / 2.0f);
+        draw_wire_box(hitbox_pos, hitbox_bounds, RED);
+
+        if (map.selected[i]) {
+			DrawTextureEx(textures.select_circle, { select_pos.x, select_pos.y }, 0, 1, WHITE);
+        }
     }
 }
 
@@ -390,10 +473,12 @@ public:
     void update(std::chrono::duration<double> delta_time);
     void init();
 private:
-    Cam cam = {{0,0}, {2,3}};
+    Cam cam = {{0,0}, {4,3}};
 
-    EditorMode mode;
+    EditorMode editor_mode = EditorMode::select;
     std::vector<int> selected;
+
+    std::optional<Vec2> box_select_begin;
 
     NoteFlags insert_flags = NoteFlagBits::don_or_kat | NoteFlagBits::normal_or_big;
     //NoteType note_type;
@@ -402,7 +487,7 @@ private:
     Map map;
     //std::vector<Note> map;
     std::chrono::duration<double> offset;
-    float bpm = 253;
+    float bpm = 60;
 
     double quarter_interval = 60 / bpm / 4;
     double collision_range = quarter_interval / 2;
@@ -424,7 +509,7 @@ void Editor::init() {
     SetMusicVolume(music, 0.2f);
 
     PlayMusicStream(music);
-    offset = std::chrono::duration<double>(0.994);
+    offset = std::chrono::duration<double>(0);
 
     PauseMusicStream(music);
 }
@@ -442,13 +527,36 @@ void Editor::update(std::chrono::duration<double> delta_time) {
     ui.input();
 
     ui.begin_group(Style{ {0,0.5} }); {
-        const char* note_color = (insert_flags & NoteFlagBits::don_or_kat) ? "Don" : "Kat";
-        ui.rect(note_color);
-        ui.button(note_color, [&]() {
-            insert_flags = insert_flags ^ NoteFlagBits::don_or_kat;
-            });
+        const char* note_color_text = (insert_flags & NoteFlagBits::don_or_kat) ? "Don" : "Kat";
 
-        ui.rect("aksjhdfkj");
+        const char* note_size_text = (insert_flags & NoteFlagBits::normal_or_big) ? "Normal" : "Big";
+
+        const char* editor_mode_text;
+        switch (editor_mode) {
+        case EditorMode::select:
+            editor_mode_text = "Select";
+            break;
+        case EditorMode::insert:
+            editor_mode_text = "Insert";
+            break;
+        }
+
+        ui.button(editor_mode_text, [&]() {
+            if (editor_mode == EditorMode::insert) {
+                editor_mode = EditorMode::select;
+            }
+            else {
+                editor_mode = EditorMode::insert;
+            }
+		});
+
+        ui.button(note_size_text, [&]() {
+            insert_flags = insert_flags ^ NoteFlagBits::normal_or_big;
+		});
+
+        ui.button(note_color_text, [&]() {
+            insert_flags = insert_flags ^ NoteFlagBits::don_or_kat;
+		});
     }
     ui.end_group();
 
@@ -466,34 +574,82 @@ void Editor::update(std::chrono::duration<double> delta_time) {
     ui.rect(frame_time.data());
     ui.end_group();
 
-    if (!ui.clicked && IsMouseButtonPressed(0)) {
-        Vec2 cursor_pos = cam.screen_to_world({(float)GetMouseX(), (float)GetMouseY()});
+	Vec2 cursor_pos = cam.screen_to_world({ (float)GetMouseX(), (float)GetMouseY() });
 
-        int i = std::round((cursor_pos.x - offset.count()) / quarter_interval);
-        float time = offset.count() + i * quarter_interval;
-
-        bool collision = false;
-        double diff;
-        for (int i = 0; i < map.times.size(); i++) {
-            diff = std::abs((map.times[i] - time));
-            if (diff < collision_range) {
-                collision = true;
-                break;
-            }
-        }
-
-        if (!collision) {
-            map.insert_note(time, insert_flags);
-
-            if (time < elapsed) {
-                current_note++;
-            }
-        }
+    if (IsKeyPressed(KEY_A)) {
+        int asdf = 0;
     }
+
+    switch (editor_mode) {
+    case EditorMode::select:
+        if (!ui.clicked && IsMouseButtonPressed(0)) {
+            std::cout << cursor_pos.x << '\n';
+            std::optional<int> to_select = note_point_intersection(map, cursor_pos, current_note);
+
+            if (to_select.has_value()) {
+                map.selected[to_select.value()] = !map.selected[to_select.value()];
+            } else { 
+                box_select_begin = cursor_pos;
+            }
+        }
+
+        if (IsMouseButtonDown(0)) {
+            if (box_select_begin.has_value()) {
+                auto hits = note_box_intersection(map, box_select_begin.value(), cursor_pos);
+
+                std::fill(map.selected.begin(), map.selected.end(), false);
+				for (auto& i : hits) {
+                    map.selected[i] = true;
+                }
+            }
+
+        }
+
+        if (IsMouseButtonReleased(0)) {
+            box_select_begin = {};
+        }
+
+        break;
+    case EditorMode::insert:
+		if (!ui.clicked && IsMouseButtonPressed(0)) {
+			Vec2 cursor_pos = cam.screen_to_world({(float)GetMouseX(), (float)GetMouseY()});
+
+			int i = std::round((cursor_pos.x - offset.count()) / quarter_interval);
+			float time = offset.count() + i * quarter_interval;
+
+			bool collision = false;
+			double diff;
+			for (int i = 0; i < map.times.size(); i++) {
+				diff = std::abs((map.times[i] - time));
+				if (diff < collision_range) {
+					collision = true;
+					break;
+				}
+			}
+
+			if (!collision) {
+				map.insert_note(time, insert_flags);
+
+				if (time < elapsed) {
+					current_note++;
+				}
+			}
+		}
+        break;
+    }
+
 
     if (IsKeyPressed(KEY_F5)) {
         Game game{};
         game.run();
+    }
+
+    if (IsKeyPressed(KEY_Q)) { 
+        editor_mode = EditorMode::select;
+    }
+
+    if (IsKeyPressed(KEY_W)) { 
+        editor_mode = EditorMode::insert;
     }
 
     if (IsKeyPressed(KEY_E)) {
@@ -547,7 +703,7 @@ void Editor::update(std::chrono::duration<double> delta_time) {
         // }
         // auto cmp = [](float current_time, Note& note) { return current_time < note.time.count(); };
         // current_note = std::upper_bound(map.begin(), map.end(), elapsed, cmp) - map.begin();
-        std::cout << current_note << '\n';
+        //std::cout << current_note << '\n';
     }
 
 
@@ -589,10 +745,29 @@ void Editor::update(std::chrono::duration<double> delta_time) {
 
     DrawLine(p1.x, p1.y, p2.x, p2.y, YELLOW);
 
-    if (IsKeyPressed(KEY_A)) {
-        int asdf = 0;
-    }
+
     draw_map_editor(map, cam, current_note);
+
+    if (box_select_begin.has_value()) {
+        Vec2 start_pos = cam.world_to_screen(box_select_begin.value());
+        Vec2 end_pos = cam.world_to_screen(cursor_pos);
+
+        if (end_pos.x < start_pos.x) {
+            float temp = start_pos.x;
+            start_pos.x = end_pos.x;
+            end_pos.x = temp;
+        }
+        
+        if (end_pos.y < start_pos.y) {
+            float temp = start_pos.y;
+            start_pos.y = end_pos.y;
+            end_pos.y = temp;
+
+        }
+
+        Vec2 scale = { end_pos.x - start_pos.x, end_pos.y - start_pos.y };
+        DrawRectangle(start_pos.x, start_pos.y, scale.x, scale.y, { 255, 255, 255, 40 });
+    }
 
 
     ui.draw();
@@ -661,6 +836,7 @@ enum class Context {
 
 void run() {
     InitWindow(window_width, window_height, "taiko");
+    //ToggleFullscreen();
     
     auto last_frame = std::chrono::high_resolution_clock::now();
     
@@ -675,6 +851,7 @@ void run() {
     textures.circle_overlay = LoadTexture("circle-overlay.png");
     textures.big_circle = LoadTexture("big-circle.png");
     textures.big_circle_overlay = LoadTexture("big-circle-overlay.png");
+    textures.select_circle = LoadTexture("circle-select.png");
     
     float pos = 400;
     auto start = std::chrono::high_resolution_clock::now();
@@ -696,28 +873,19 @@ void run() {
     using namespace std::chrono;
     
     while (!WindowShouldClose()) {
-
-        //BeginDrawing();
-        //ClearBackground(BLACK);
-
-        //DrawCircle(600, 500, 100, Color{ 0, 0, 255, 128 });
-        //DrawCircle(500, 500, 100, Color{ 255, 0, 0, 128 });
-
-        //EndDrawing();
-
         auto now = high_resolution_clock::now();
         duration<double> delta_time = now - last_frame;
 
         switch (context) {
-            case Context::Menu:
-                menu.update(to_editor);
-                break;
-            case Context::Editor:
-                editor.update(delta_time);
-                break;
-            case Context::Game:
-                game.update(delta_time);
-                break;
+		case Context::Menu:
+			menu.update(to_editor);
+			break;
+		case Context::Editor:
+			editor.update(delta_time);
+			break;
+		case Context::Game:
+			game.update(delta_time);
+			break;
         }
 
         last_frame = now;
